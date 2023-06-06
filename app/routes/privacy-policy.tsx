ab505acbf51f898a3bc56a0b24173bb1845c335f
th-m <thomvaladez@gmail.com>
2023-06-05 19:48:42 -0600

    init

---
+++ app/routes/privacy-policy.tsx
@@ -0,0 +1,57 @@
+export default function Index() {
+    return (
+      <>
+        <h2>Cookies</h2>
+        <p>
+          {" "}
+          Cookies are small files saved on your computer to store data about your
+          connections. This site uses cookies to facilitate the order process and
+          display language and currency.
+        </p>
+  
+        <p>
+          The site also uses Google Analytics analysis tool that collects
+          information about your browsing with cookies. These data are collected
+          for the purposes of statistical analysis and are confidential and
+          anonymous.
+        </p>
+  
+        <h2>Disable Cookies</h2>
+        <p>
+          You can disable cookies by changing the preferences in your browser, but
+          this may prevent the normal functioning of the site.
+        </p>
+  
+        <p>To block or enable cookies</p>
+        <ul>
+          <li>- Internet Explorer</li>
+          <li>- Firefox</li>
+          <li>- Chrome</li>
+          <li>- Safari</li>
+        </ul>
+  
+        <h2>Data Gathered When Creating An Account And Order</h2>
+        <p>
+          When you create an account on this site, we store the information needed
+          for orders in a client file. These data are confidential and will not be
+          disclosed to third parties. You can at any time request to view, edit or
+          delete your client informations by sending us your request by mail or
+          email with a copy of your ID.
+        </p>
+  
+        <h2>3rd Party Authentication</h2>
+        <p>
+          SoundSculpt use of information received from Google APIs will adhere to
+          Google API Services User Data Policy, including the Limited Use
+          requirements.
+        </p>
+  
+        <p>
+          SoundSculpt use of information received from Facebook APIs will adhere
+          to Facebook API Services User Data Policy, including the Limited Use
+          requirements.
+        </p>
+      </>
+    );
+  }
+  
